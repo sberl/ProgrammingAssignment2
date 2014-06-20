91e3b729b39d6aecbe3635716f7d4283afd6e769
Steve Berl <steveberl@gmail.com>
2014-06-19 18:56:37 -0700

    First cut at homework assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a special version of a matrix data structure, which is able to
+## cache the value of the inverse of a matrix in order to improve performance
+## of programs which need to solve for the inverse of a matrix multiple
+## times.
 
-## Write a short comment describing this function
+## Use this function to initialize the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInv <- NULL
+    set <- function(y) {
+        x <<- y
+        cachedInv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) cachedInv <<- inv
+    getinverse <- function() cachedInv
+    
+    list(set = set, 
+         get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function will return the inverse of the matrix. It will either
+## retrieve it from the cache, or solve it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Check for a cached value
+    cachedval <- x$getinverse()
+    if (!is.null(cachedval)) {
+        # There was a cached value, and the matrix has not changed?
+        message("getting cached data")
+        cachedval # return cached inverse
+    } else {
+        # no cached value
+        # Need to recalculate
+        data <- x$get()
+        inv <- solve(data)
+        
+        # Save solution in case they ask again
+        x$setinverse(inv)
+        inv  # return inverse
+    }
 }
